417bd429387b08556cf2496fc7b126994780e877
melvinchien <melvin.chien@gmail.com>
2012-10-25 18:49:23 -0700

    Started yet again, without 320 and Up Boilerplate

--- AN/js/main.js
+++ AN/js/main.js
@@ -10,11 +10,10 @@ function loadMap() {
         mapTypeId: 'roadmap'
     };
     map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-    var infoWindow = new google.maps.InfoWindow;
     
     //var geocoder = new google.maps.Geocoder();
     
-    downloadUrl("andb-connect.php", function(data) {});
+    downloadUrl("andb-connect.php", displayMarkers(data));
 }
 
 function downloadUrl(url, callback) {
@@ -34,6 +33,41 @@ function downloadUrl(url, callback) {
 
     request.open('GET', url, true);
     request.send(null);
+}
+
+function displayMarkers(data) {
+    var xml = data.responseXML;
+    var alumni = xmlDoc.getElementsByTagName("alumnus");
+    for (var i = 0; i < alumni.length; i++) {
+        var first_name = alumni[i].getAttribute("First_Name");
+        var last_name = alumni[i].getAttribute("Last_Name");
+        var bus_name = alumni[i].getAttribute("Business_Name");
+        var bus_lat = alumni[i].getAttribute("Business_Lat");
+        var bus_lng = alumni[i].getAttribute("Business_Lng");
+        createMarker(first_name, last_name, bus_name, bus_lat, bus_lng);
+    }
+}
+
+function createMarker(first_name, last_name, bus_name, bus_lat, bus_lng) {
+    var point = new google.maps.LatLng(parseFloat(bus_lat), parseFloat(bus_lng));
+    var marker = new google.maps.Marker({
+        map: map,
+        animation: google.maps.Animation.Drop,
+        position: point,
+        title: bus_name
+    });
+		  
+    var contentString = '<div id="content">' + '<h2 id="firstHeading" class="firstHeading">' + bus_name + '</h2>' + '<div id="bodyContent">' + '<p>' + first_name + ' ' + last_name;
+		  
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+		  
+    if (infowindow) infowindow.close();
+    google.maps.event.addListener(marker, "click", function() {
+        infowindow.open(map, marker);
+    });
+    markers.push(marker);
 }
 
 function doNothing() {}
