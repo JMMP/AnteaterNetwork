3a1e39e65b582c0aa00d07a208b6ae0b92049707
melvinchien <melvin.chien@gmail.com>
2012-10-28 01:40:12 -0700

    Version five point zero! Progress!
    Added map styling from Patrick
    Adjusted default zoom
    Added custom marker icon
    Added filter functionality for business names, cities, and zip code
    (zip code is only a direct match)
    Combined multiple JavaScript filters into one populate() function
    Added (hardcoded) functionality for more than one filter being applied
    at the same time
    Fixed markers' info windows not being closed automatically
    Added more information inside info windows

--- 5-five/main.js
+++ 5-five/main.js
@@ -1,94 +1,123 @@
 var map;
 var markers = [];
 var xmlDoc;
+var request = "";
+var infowindow;
+
+// Style Google Maps
+var markerImage = "images/marker_anteater_small.png";
+
+// https://developers.google.com/maps/documentation/javascript/styling
+var stylesArray = [{
+   "featureType": "poi.school",
+   "stylers": [{
+      "hue": "#eeff00"
+   },{
+      "saturation": 56
+   }]
+},{
+   "stylers": [{
+      "lightness": -21
+   }]
+},{
+   "featureType": "road",
+   "stylers": [{
+      "hue": "#0000ff"
+   },{
+      "weight": 0.4
+   }]
+}];
+
 function loadMap() {
    var myOptions = {
       center: new google.maps.LatLng(33.646259, -117.842056),
-      zoom: 10,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
+      zoom: 12,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      styles: stylesArray
    };
 		
    map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
 		
    var geocoder = new google.maps.Geocoder();
-   populate("");
+   
 }
 
 	  
-function populate(city) {
-   try {
-      var xmlhttp;
-      if (window.XMLHttpRequest) {
-         xmlhttp = new XMLHttpRequest();
-      } else {
-         xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-      }
-      xmlhttp.onreadystatechange = function() {
-         if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-            xmlDoc = xmlhttp.responseXML;
-            clearMarkers();
-            var alumni = xmlDoc.getElementsByTagName("alumnus");
-            for (var i = 0; i < alumni.length; i++) {
-               var first_name = alumni[i].getAttribute("First_Name");
-               var last_name = alumni[i].getAttribute("Last_Name");
-               var bus_name = alumni[i].getAttribute("Business_Name");
-               var bus_lat = alumni[i].getAttribute("Business_Lat");
-               var bus_lng = alumni[i].getAttribute("Business_Lng");
-               createMarker(first_name, last_name, bus_name, bus_lat, bus_lng);
-            }
+function populate(filter, input) {
+   var xmlhttp;
+   if (window.XMLHttpRequest) {
+      xmlhttp = new XMLHttpRequest();
+   } else {
+      xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+   }
+   xmlhttp.onreadystatechange = function() {
+      if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+         xmlDoc = xmlhttp.responseXML;
+         clearMarkers();
+         var alumni = xmlDoc.getElementsByTagName("alumnus");
+         for (var i = 0; i < alumni.length; i++) {
+            
+            createMarker(alumni[i]);
          }
       }
-      
-      xmlhttp.open("GET", "andb-connect.php", true);
-      xmlhttp.send();
-   } catch (e) {
-      alert("Error: " + e);
    }
-}
-	
-function filterCity(input) {
-   clearMarkers();
-   var alumni = xmlDoc.getElementsByTagName("alumnus");
-   for (var i = 0; i < alumni.length; i++) {
-      var bus_city = alumni[i].getAttribute("Business_City");
-      if (input == "" | bus_city == input) {
-         var first_name = alumni[i].getAttribute("First_Name");
-         var last_name = alumni[i].getAttribute("Last_Name");
-         var bus_name = alumni[i].getAttribute("Business_Name");
-         var bus_lat = alumni[i].getAttribute("Business_Lat");
-         var bus_lng = alumni[i].getAttribute("Business_Lng");
-         createMarker(first_name, last_name, bus_name, bus_lat, bus_lng);
-      }
-   }
+   
+   if (filter != "") 
+      request += "&";
+   if (filter == "city")
+      request += "city=" + input;
+   else if (filter == "name")
+      request += "name=" + input;
+   else if (filter == "zipcode")
+      request += "zipcode=" + input;
+   
+   xmlhttp.open("GET", "andb-connect.php?" + request, true);
+   xmlhttp.send();
 }
 
-function filterName(input) {
    
-}
+function createMarker(alumni) {
+   var first_name = alumni.getAttribute("First_Name");
+   var last_name = alumni.getAttribute("Last_Name");
+   var class_year = alumni.getAttribute("Class_Year");
+   var school_code = alumni.getAttribute("School_Code");
+   var bus_title = alumni.getAttribute("Business_Title");
+   var bus_name = alumni.getAttribute("Business_Name");
+   var bus_street1 = alumni.getAttribute("Business_Street1");
+   var bus_street2 = alumni.getAttribute("Business_Street2");
+   var bus_city = alumni.getAttribute("Business_City");
+   var bus_state = alumni.getAttribute("Business_State");
+   var bus_zipcode = alumni.getAttribute("Business_Zipcode");
+   var bus_phone = alumni.getAttribute("Business_Phone");
+   var bus_lat = alumni.getAttribute("Business_Lat");
+   var bus_lng = alumni.getAttribute("Business_Lng");
    
-function createMarker(first_name, last_name, bus_name, bus_lat, bus_lng) {
    var point = new google.maps.LatLng(parseFloat(bus_lat), parseFloat(bus_lng));
    var marker = new google.maps.Marker({
       map: map,
-      animation: google.maps.Animation.Drop,
+      animation: google.maps.Animation.DROP,
       position: point,
+      icon: markerImage,
       title: bus_name
    });
 		  
-   var contentString = '<div id="content">' + '<h2 id="firstHeading" class="firstHeading">' + bus_name + '</h2>' + '<div id="bodyContent">' + '<p>' + first_name + ' ' + last_name;
+   var contentString = "<div id='infoWindow'>" + 
+      "<h2 id='firstHeading' class='firstHeading'>" + bus_name + "</h2>" + 
+      "<div id='bodyContent'>" + "<p>" + first_name + " " + last_name + 
+      " " + school_code + " " + class_year + "</p>" + "<p>" + bus_name + "<br />" + 
+      bus_street1 + "<br />" + bus_city + ", " + bus_state + " " + bus_zipcode + 
+      "<br />" + bus_phone + "</p>";
 		  
-   var infowindow = new google.maps.InfoWindow({
+   
+   google.maps.event.addListener(marker, "click", function() {
+      if (infowindow) infowindow.close();
+      infowindow = new google.maps.InfoWindow({
       content: contentString
    });
-		  
-   if (infowindow) infowindow.close();
-   google.maps.event.addListener(marker, "click", function() {
       infowindow.open(map, marker);
    });
    
-   
-      markers.push(marker);
-   
+   markers.push(marker);
 }
 	  
 	  
