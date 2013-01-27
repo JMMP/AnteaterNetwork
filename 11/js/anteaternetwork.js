788956423cb4e9b86515e5282b22f3bdb4fa430a
suuoh <melvin.chien@gmail.com>
2013-01-27 15:45:56 -0800

    Set up results list and two column layout

--- 11/js/anteaternetwork.js
+++ 11/js/anteaternetwork.js
@@ -1,10 +1,59 @@
 var map;
+var markerImage;
+var mapStyles;
 
 $(document).ready( function() {
   loadMap();
+
+  $('#js-toggle-clusters').on('switch-change', function (e, data) {
+    var $el = $(data.el);
+    var value = data.value;
+    console.log(e, $el, value);
+  });
 });
 
+// Catch enter presses on main page
+function enterPressed(e) {
+  var keycode;
+  if (window.event)
+    keycode = window.event.keyCode;
+  else if (e)
+    keycode = e.which;
+  else
+    return false;
+  return (keycode == 13);
+}
+
 function loadMap() {
+  markerImage = "images/marker_anteater_small.png";
+  mapStyles = [{
+    "featureType": "water",
+    "stylers": [{
+      "lightness": 14
+    }]
+  }, {
+    "featureType": "road",
+    "stylers": [{
+      "saturation": 100
+    }, {
+      "weight": 0.3
+    }, {
+      "hue": "#002bff"
+    }]
+  }, {
+    "featureType": "landscape",
+    "stylers": [{
+      "hue": "#00ff44"
+    }]
+  }, {
+    "featureType": "poi.school",
+    "stylers": [{
+      "saturation": 100
+    }, {
+      "hue": "#ffe500"
+    }]
+  }];
+
   map = new GMaps({
     el: "#map_canvas",
     lat: 33.646259,
@@ -16,27 +65,9 @@ function loadMap() {
     },
     panControl: false,
     scaleControl: false,
+    styles: mapStyles
     // markerClusterer: function(map) {
     //   return new MarkerClusterer(map);
     // }
   });
 };
-
-
-// Catch enter presses on main page
-function enter_pressed(e) {
-  var keycode;
-  if (window.event)
-    keycode = window.event.keyCode;
-  else if (e)
-    keycode = e.which;
-  else
-    return false;
-  return (keycode == 13);
-}
-
-$('#js-toggle-clusters').on('switch-change', function (e, data) {
-    var $el = $(data.el);
-    var value = data.value;
-    console.log(e, $el, value);
-});
