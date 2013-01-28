6166d0f82d2faa02d0c8851ef83c42ceceac7eba
suuoh <melvin.chien@gmail.com>
2013-01-27 17:24:46 -0800

    Version 9.0

--- js/main.js
+++ js/main.js
@@ -1,22 +1,27 @@
 var map;
+var mc;
 var markers = [];
 var markersLatLng = [];
-var xmlhttp;
+var xmlhttpMarkers;
 var xmlDoc;
+var xmlhttpMenus;
 var filters = [
 ["city", ""],
 ["name", ""],
 ["zipcode", ""]];
-var infowindow;
-var sideboxhtml = "";
+var sidenav;
 var pinDrop = false;
+var latlngBuffer = 0.003;
+var clusters = false;
+var infoWindow;
+var gc;
 
 // Custom marker image
 var markerImage = "images/marker_anteater_small.png";
 
 // Style Google Maps
 // https://developers.google.com/maps/documentation/javascript/styling
-var stylesArray = [{
+var mapStyles = [{
     "featureType": "water",
     "stylers": [{
         "lightness": 14
@@ -44,14 +49,14 @@ var stylesArray = [{
     }]
 }];
 
-
 function loadMap(divID) {
+
     var myOptions = {
         center: new google.maps.LatLng(33.646259, -117.842056),
         zoom: 12,
         mapTypeId: google.maps.MapTypeId.ROADMAP,
-        styles: stylesArray,
-		
+        styles: mapStyles,
+
         panControl: true,
         panControlOptions: {
             position: google.maps.ControlPosition.TOP_RIGHT
@@ -61,103 +66,90 @@ function loadMap(divID) {
             style: google.maps.ZoomControlStyle.LARGE,
             position: google.maps.ControlPosition.TOP_RIGHT
         }
-		
+
     };
 
     map = new google.maps.Map(document.getElementById(divID), myOptions);
-	
-	mc = new MarkerClusterer(map);
-
-//var geocoder = new google.maps.Geocoder();
+    mc = new MarkerClusterer(map);
+    sidenav = document.getElementById("sidenav");
+    google.maps.event.addListenerOnce(map, 'tilesloaded', function(){
+        getMenu('city');
+        populate('', '');
+    });
+//gc = new google.maps.Geocoder();
 
 }
-
 
 function populate(filter, input) {
-    
+    var start = new Date();
+    setFilter(filter, input);
+    sidenav.innerHTML = "";
+    clearMarkers();
     createXMLHttpRequest(function() {
-        updatePinDrop(filter);
-        xmlDoc = xmlhttp.responseXML;
-        clearMarkers();
-        sideboxhtml = "";
-        var alumni = parseXML(xmlDoc);
-        for (i in alumni) {
-            var marker = createMarker(alumni[i]);
-            if (marker)
-                markers.push(marker);
+        //pinDrop = true;
+        xmlDoc = xmlhttpMarkers.responseXML;
+        var alumni = xmlDoc.getElementsByTagName("alumnus");
+        for (i = 0; i < alumni.length; i++) {
+            createMarker(alumni[i]);
         }
+        if (clusters) {
+            clusters = false;
+            toggleClusters();
+        }
+        setBounds();
     });
-    
-    setBounds();
-    
-    sendXMLHttpRequest(getRequest(filter, input));
-}
 
+    xmlhttpMarkers.open("GET", "getAlumni.php" + getRequest(), true);
+    xmlhttpMarkers.send();
+    google.maps.event.addListenerOnce(map, 'tilesloaded', function(){
+        var finish = new Date();
+        document.getElementById("timetaken").innerHTML = finish - start;
+    });
 
-function sendXMLHttpRequest(request) {
-    xmlhttp.open("GET", "andb-connect.php?" + request, true);
-    xmlhttp.send();
 }
 
 
 function createXMLHttpRequest(callback) {
     if (window.XMLHttpRequest) {
         // IE7+, Firefox, Chrome, Opera, Safari
-        xmlhttp = new XMLHttpRequest();
+        xmlhttpMarkers = new XMLHttpRequest();
     } else {
         // IE6, IE5
-        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        xmlhttpMarkers = new ActiveXObject("Microsoft.XMLHTTP");
     }
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+
+    xmlhttpMarkers.onreadystatechange = function() {
+        if (xmlhttpMarkers.readyState == 4 && xmlhttpMarkers.status == 200) {
             callback();
         }
     };
 }
 
 
-function updatePinDrop(filter) {
-    // Set drop animation if there is no filter
-    // This occurs on first load and on filter reset
-    if (filter == "")
-        pinDrop = true;
-    else 
-        pinDrop = false;
-}
-
-
-function parseXML(xml) {
-    return xml.getElementsByTagName("alumnus");
-}
-
-
-function setBounds() {
-    //  Create a new viewpoint bound
-    var bounds = new google.maps.LatLngBounds();
-	
-
-    // If array is empty, focus map around UCI
-    if (markersLatLng.length == 0) {
-        markersLatLng.push(new google.maps.LatLng(33.70095,-117.710438));
-        markersLatLng.push(new google.maps.LatLng(33.576899,-117.978916));
-    }
-
-    // Increase bounds for each marker
-    for (i in markersLatLng) {
-        bounds.extend(markersLatLng[i]);
-    }
-
-    // Fit bounds to the map
-    map.fitBounds(bounds);
-}
-
-
-function getRequest(filter, input) {
+function setFilter(filter, input) {
     for (i in filters) {
         if (filters[i][0] == filter)
             filters[i][1] = input;
     }
+}
 
+
+function resetFilter(filter) {
+    for (i in filters) {
+        if (filters[i][0] == filter)
+            filters[i][1] = "";
+    }
+}
+
+
+function resetFilters() {
+    for (i in filters) {
+        filters[i][1] = "";
+    }
+}
+
+
+function getRequest() {
     var request = "";
     for (i in filters) {
         if (filters[i][1] != "") {
@@ -166,78 +158,143 @@ function getRequest(filter, input) {
             request += filters[i][0] + "=" + filters[i][1];
         }
     }
-    return request;
+    return "?" + request;
 }
 
 
 function createMarker(alumni) {
-    var first_name = alumni.getAttribute("First_Name");
-    var last_name = alumni.getAttribute("Last_Name");
-    var class_year = alumni.getAttribute("Class_Year");
-    var school_code = alumni.getAttribute("School_Code");
-    var bus_title = alumni.getAttribute("Business_Title");
-    var bus_name = alumni.getAttribute("Business_Name");
-    var bus_street1 = alumni.getAttribute("Business_Street1");
-    var bus_street2 = alumni.getAttribute("Business_Street2");
-    var bus_city = alumni.getAttribute("Business_City");
-    var bus_state = alumni.getAttribute("Business_State");
-    var bus_zipcode = alumni.getAttribute("Business_Zipcode");
-    var bus_phone = alumni.getAttribute("Business_Phone");
-    var bus_lat = alumni.getAttribute("Business_Lat");
-    var bus_lng = alumni.getAttribute("Business_Lng");
+    var sideListing = document.createElement("LI");
+    var sideItem = document.createElement("A");
+    var sideDetails = document.createElement("SPAN");
+    var sideHTML = "";
+    var busLat = "";
+    var busLng = "";
+    var busName = "";
 
+    var infoHTML = "<div class='infoWindow'>";
+
+    if (alumni.hasAttribute("Business_Name")) {
+        busName = alumni.getAttribute("Business_Name");
+        sideItem.innerHTML = busName;
+        infoHTML += "<h2 id='firstHeading' class='firstHeading'>" + busName + "</h2>";
+    }
+
+    infoHTML += "<div id='bodyContent'>";
+
+    if (alumni.hasAttribute("First_Name") && alumni.hasAttribute("Last_Name")) {
+        var firstName = alumni.getAttribute("First_Name");
+        var lastName = alumni.getAttribute("Last_Name");
+        infoHTML += firstName + " " + lastName  + "<br />";
+    }
+
+    if (alumni.hasAttribute("Business_Street1")) {
+        var busStreet1 = alumni.getAttribute("Business_Street1");
+        sideHTML += busStreet1 + "<br />";
+        infoHTML += busStreet1 + "<br />";
+    }
+
+    if (alumni.hasAttribute("Business_City") && alumni.hasAttribute("Business_State")) {
+        var busCity = alumni.getAttribute("Business_City");
+        var busState = alumni.getAttribute("Business_State");
+        sideHTML += busCity + ", " + busState;
+        infoHTML += busCity + ", " + busState;
+    }
+
+    if (alumni.hasAttribute("Business_Zipcode")) {
+        var busZipcode = alumni.getAttribute("Business_Zipcode");
+        sideHTML += " " + busZipcode;
+        infoHTML += " " + busZipcode;
+    }
+
+    sideHTML += "<br />";
+    infoHTML += "<br />";
+
+    if (alumni.hasAttribute("Business_Phone")) {
+        var busPhone = alumni.getAttribute("Business_Phone");
+        sideHTML += busPhone;
+        infoHTML += busPhone + "<br />";
+    }
+
+    if (alumni.hasAttribute("Business_Lat") && alumni.hasAttribute("Business_Lng")) {
+        busLat = alumni.getAttribute("Business_Lat");
+        busLng = alumni.getAttribute("Business_Lng");
+    }
+
+    sideDetails.innerHTML = sideHTML;
+    sideItem.appendChild(sideDetails);
 
     // Generate HTML list for the results list on the side
-    sideboxhtml +="<li> <a href='#'>" + bus_name + "<br/><span>" + bus_street1 + "<br />";
-    sideboxhtml += bus_city + ", " + bus_state + " " + bus_zipcode + "<br />" + bus_phone + "</span> </a> </li>";
-    var sidebox = document.getElementById("sidenav");
-    sidebox.innerHTML = sideboxhtml;
-    
+    sideListing.appendChild(sideItem);
+    sidenav.appendChild(sideListing);
+
+    //var point = codeAddress(busStreet1 + ", " + busCity + ", " + busState);  // Geocoding
+
     // Catch businesses with no latitude or longitude
     // Don't show these on the map but still list them in the results box
-    if (bus_lat != "" || bus_lng != "") {
-        var point = new google.maps.LatLng(parseFloat(bus_lat), parseFloat(bus_lng));
+    if (busLat != "" || busLng != "") {
+        //if (point) {  // Geocoding
+        var point = new google.maps.LatLng(parseFloat(busLat), parseFloat(busLng));
+        //busLat = point.lat();  // Geocoding
+        //busLng = point.lng();  // Geocoding
+
+        var pointBufferedNE = new google.maps.LatLng(parseFloat(busLat) + latlngBuffer, parseFloat(busLng) + latlngBuffer);
+        var pointBufferedSW = new google.maps.LatLng(parseFloat(busLat) - latlngBuffer, parseFloat(busLng) - latlngBuffer);
 
         // Add marker position to array
-        markersLatLng.push(point);
+        markersLatLng.push(pointBufferedNE);
+        markersLatLng.push(pointBufferedSW);
 
         var marker = new google.maps.Marker({
             map: map,
             position: point,
             icon: markerImage,
-            title: bus_name
+            title: busName
         });
-        
+
         // Do not have any animation when filtering
         if (pinDrop)
             marker.setAnimation(google.maps.Animation.DROP);
         else
             marker.setAnimation();
 
+        infoHTML += "<a href='http://maps.google.com/maps?daddr=" + point.toUrlValue() + "' target ='_blank'>Get Directions</a>";
 
-        var contentString = "<div id='infoWindow'>" +
-        "<h2 id='firstHeading' class='firstHeading'>" + bus_name + "</h2>" +
-        "<div id='bodyContent'>" + "" + first_name + " " + last_name +
-        " (" + school_code + ", " + class_year + ")<br />" + bus_title + "<br />" + bus_name + "<br />" +
-        bus_street1 + "<br />" + bus_city + ", " + bus_state + " " + bus_zipcode +
-        "<br />" + bus_phone + "<br />";
-
-
-        google.maps.event.addListener(marker, "click", function() {
-            if (infowindow) infowindow.close();
-            infowindow = new google.maps.InfoWindow({
-                content: contentString
-            });
-            infowindow.open(map, marker);
-        });
-
+        google.maps.event.addListener(marker, "click", busClick(infoHTML, marker));
+        google.maps.event.addDomListener(sideItem, "click", busClick(infoHTML, marker));
+        markers.push(marker);
         return(marker);
     }
-    
     return false;
+}
 
-    
+function busClick(html, marker) {
+    return function() {
+        if (infoWindow)
+            infoWindow.close();
+        infoWindow = new google.maps.InfoWindow();
+        infoWindow.setContent(html);
+        infoWindow.open(map, marker);
+    }
+}
 
+function setBounds() {
+    if (markers.length == 0) {
+        // If there are no markers to show, don't move map and give an alert or error instead
+        document.getElementById("debug").innerHTML = "No markers to display!";
+        return false;
+    } else {
+        //  Create a new viewpoint bound
+        var bounds = new google.maps.LatLngBounds();
+        // Increase bounds for each marker
+        for (i in markersLatLng) {
+            bounds.extend(markersLatLng[i]);
+        }
+
+        // Fit bounds to the map
+        map.fitBounds(bounds);
+        document.getElementById("debug").innerHTML = "";
+        return true;
+    }
 }
 
 
@@ -251,35 +308,76 @@ function clearMarkers() {
     markersLatLng = [];
 }
 
-// Incomplete
-function codeAddress() {
-    var address;
-    geocoder.geocode(address, function(results, status) {
+
+function hideMarkers() {
+    for (i in markers) {
+        markers[i].setVisible(false);
+    }
+}
+
+
+function showMarkers() {
+    for (i in markers) {
+        markers[i].setVisible(true);
+        markers[i].setMap(map);
+    }
+}
+
+
+function codeAddress(input) {
+    var gcrequest = {
+        address: input,
+        region: "US"
+    };
+    gc.geocode(gcrequest, function(results, status) {
         if (status == google.maps.GeocoderStatus.OK) {
-            var marker = new google.maps.Marker({
-                map: map,
-                position: results[0].geomtery.location
-            });
+            return results[0].geometry.location;
         } else {
-            if (debug) alert("Geocode was note successful: " + status);
+            alert(input + "\n" + "Geocode was note successful: " + status);
         }
     });
+    return false;
 }
 
-function resetFilters() {
-    clearMarkers();
-    for (i in filters) {
-        filters[i][1] = "";
+// Catch enter presses on main page
+function enter_pressed(e) {
+    var keycode;
+    if (window.event)
+        keycode = window.event.keyCode;
+    else if (e)
+        keycode = e.which;
+    else
+        return false;
+    return (keycode == 13);
+}
+
+function getMenu(menu) {
+    if (window.XMLHttpRequest) {
+        // IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttpMenus = new XMLHttpRequest();
+    } else {
+        // IE6, IE5
+        xmlhttpMenus = new ActiveXObject("Microsoft.XMLHTTP");
     }
-    populate("", "");
+
+    xmlhttpMenus.onreadystatechange = function() {
+        if (xmlhttpMenus.readyState == 4 && xmlhttpMenus.status == 200) {
+            document.getElementById("submenu_city").innerHTML = xmlhttpMenus.responseText;
+        }
+    };
+
+    xmlhttpMenus.open("GET", "getMenu.php?menu=" + menu, true);
+    xmlhttpMenus.send();
 }
 
-function clusterMarkers() {
-    
-   mc.addMarkers(markers);
-}
 
-function clearCluster() {
-	mc.clearMarkers(markers);
-	//mc.resetViewport_(true);
+function toggleClusters() {
+    mc.clearMarkers();
+    if (clusters) {
+        showMarkers();
+        clusters = false;
+    } else {
+        mc.addMarkers(markers);
+        clusters = true;
+    }
 }
