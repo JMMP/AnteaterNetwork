5a96dcaa96741c45e3a57b022ee6ba636e218f23
suuoh <melvin.chien@gmail.com>
2013-08-12 01:25:04 -0400

    Added data retrieval and marker and info window creation
    Removed Filter.js and GMaps.js

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ session_start();
 /*
 + Anteater Network v15.0
 + Copyright 2013 JMMP and UCI Alumni Association
-+ http://alumni.uci.edu/anteater-network/
++ http://alumni.uci.edu/anteater-network
 */
 
 $_SESSION["ip"] = $_SERVER["REMOTE_ADDR"];
--- js/antnet.js
+++ js/antnet.js
@@ -1,16 +1,14 @@
 /*
  + Anteater Network v15.0
  + Copyright 2013 JMMP and UCI Alumni Association
- + http://alumni.uci.edu/anteater-network/
+ + http://alumni.uci.edu/anteater-network
  */
 
 AntNet = {
 
+  alumni: null,
+  infowindow: null,
   map: null,
-  mapCenter: {
-    "lat": 33.646259,
-    "lng": -117.842056,
-  },
   mapStyles: [{
     "featureType": "water",
     "stylers": [{
@@ -39,7 +37,7 @@ AntNet = {
     }]
   }],
   mapOptions: {
-    center: new google.maps.LatLng(this.mapCenter.lat, this.mapCenter.lng),
+    center: new google.maps.LatLng(33.646259, -117.842056),
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     zoom: 15,
     panControl: false,
@@ -56,10 +54,53 @@ AntNet = {
   markerImageBusiness: "img/marker_anteater.png",
   markerImageUser: "img/marker_person.png",
   
+  /*
+   *
+   * Functions
+   *
+   */
 
   init: function() {
-    this.map = new google.maps.Map(document.getElementById("js-map"), mapOptions);
+    this.map = new google.maps.Map(document.getElementById("js-map"), this.mapOptions);
+    this.markers = [];
+    this.infowindow = new google.maps.InfoWindow();
+    this.getAlumni();
+    
+  },
+
+  getAlumni: function() {
+    var that = this;
+    $.getJSON("getAlumni.php?antnet")
+    .fail(function() { console.log("getAlumni error"); })
+    .done(function(data) {
+      that.alumni = data;
+      $.each(that.alumni, function(i, alumnus) {
+        that.createMarker(alumnus);
+      });
+    });
+  },
+
+  createMarker: function(alumnus) {
+    var marker = new google.maps.Marker({
+      map: this.map,
+      position: new google.maps.LatLng(alumnus.Business_Lat, alumnus.Business_Lng),
+      title: alumnus.Business_Name,
+      icon: this.markerImageBusiness
+    });
+
+    var infowindowContent = document.createElement("div");
+    infowindowContent.className = "js-map-infowindow";
+    infowindowContent.innerHTML = alumnus.First_Name + alumnus.Last_Name;
+
+    var that = this;
+    google.maps.event.addListener(marker, "click", function() {
+      that.infowindow.setContent(infowindowContent);
+      that.infowindow.open(that.map, marker);
+    })
+
+    this.markers[alumnus.id] = marker;
   }
+
 
 };
 
