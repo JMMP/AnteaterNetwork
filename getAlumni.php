59ffadbf1f6a7310307421a31f3527a3d02b0eb3
suuoh <melvin.chien@gmail.com>
2013-08-11 23:56:42 -0400

    Remove business array and add GET parameter

--- getAlumni.php
+++ getAlumni.php
@@ -2,28 +2,30 @@
 session_start();
 
 /*
-+ Anteater Network v15.0
-+ Copyright 2013 JMMP and UCI Alumni Association
-+ http://alumni.uci.edu/anteater-network/
+ + Anteater Network v15.0
+ + Copyright 2013 JMMP and UCI Alumni Association
+ + http://alumni.uci.edu/anteater-network/
 */
 
-require_once("../../antnet_secure.php");
+if (isset($_GET["antnet"])) {
+  require_once("../../antnet_secure.php");
 
 // Execute query to select all rows
-$result = mysqli_query($mysqli, "SELECT * FROM `" . $table . "` ORDER BY `Business_Name`");
+  $result = mysqli_query($mysqli, "SELECT * FROM `" . $table . "` ORDER BY `Business_Name`");
 
-if (!$result) {
-  if ($debug)
-    echo "Query failed: " . mysqli_error($mysqli);
-  die();
+  if (!$result) {
+    if ($debug)
+      echo "Query failed: " . mysqli_error($mysqli);
+    die();
+  }
+
+  $businesses = array();
+
+  while ($row = mysqli_fetch_assoc($result)) {
+    $businesses[] = $row;
+  }
+
+  echo json_encode($businesses);
+  mysqli_close($mysqli);
 }
-
-$businesses = array();
-
-while ($row = mysqli_fetch_assoc($result)) {
-  $businesses[] = array("business" => $row);
-}
-
-echo json_encode($businesses);
-mysqli_close($mysqli);
 ?>
