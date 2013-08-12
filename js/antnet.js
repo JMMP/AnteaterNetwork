c5a017ae7ae452f687ec1b9b4376cd366c1c77f4
suuoh <melvin.chien@gmail.com>
2013-08-11 23:03:39 -0400

    Clean up JavaScript!

--- js/antnet.js
+++ js/antnet.js
@@ -4,87 +4,66 @@
  + http://alumni.uci.edu/anteater-network/
  */
 
-AntNet = function () {
+AntNet = {
 
-  /*
-   *
-   * Private variables
-   *
-   */
-
-  google.maps.visualRefresh = true;
-  var mapStyles = [{
-      "featureType": "water",
-      "stylers": [{
-          "lightness": 14
-        }]
+  map: null,
+  mapCenter: {
+    "lat": 33.646259,
+    "lng": -117.842056,
+  },
+  mapStyles: [{
+    "featureType": "water",
+    "stylers": [{
+      "lightness": 14
+    }]
+  }, {
+    "featureType": "road",
+    "stylers": [{
+      "saturation": 100
     }, {
-      "featureType": "road",
-      "stylers": [{
-          "saturation": 100
-        }, {
-          "weight": 0.3
-        }, {
-          "hue": "#002bff"
-        }]
+      "weight": 0.3
     }, {
-      "featureType": "landscape",
-      "stylers": [{
-          "hue": "#00ff44"
-        }]
+      "hue": "#002bff"
+    }]
+  }, {
+    "featureType": "landscape",
+    "stylers": [{
+      "hue": "#00ff44"
+    }]
+  }, {
+    "featureType": "poi.school",
+    "stylers": [{
+      "saturation": 100
     }, {
-      "featureType": "poi.school",
-      "stylers": [{
-          "saturation": 100
-        }, {
-          "hue": "#ffe500"
-        }]
-    }];
+      "hue": "#ffe500"
+    }]
+  }],
+  mapViewBuffer: 0.0035,
+  markers: null,
+  markerClusterer: null,
+  markerImageBusiness: "img/marker_anteater.png",
+  markerImageUser: "img/marker_person.png",
+  
 
-  var gmap;
-  var markers;
+  init: function() {
+    this.map = new google.maps.Map(document.getElementById("js-map"), {
+      center: new google.maps.LatLng(this.mapCenter.lat, this.mapCenter.lng),
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      zoom: 15,
+      panControl: false,
+      zoomControl: true,
+      zoomControlOptions: {
+        position: google.maps.ControlPosition.TOP_RIGHT,
+        style: google.maps.ZoomControlStyle.LARGE
+      },
+      styles: this.mapStyles
+    });
+  }
 
-  /*
-   *
-   * Private functions
-   *
-   */
-
-  return {
-
-    /*
-     *
-     * Public variables
-     *
-     */
-
-    Map: gmap,
-    Markers: markers,
-
-    /*
-     *
-     * Public functions
-     *
-     */
-
-    init: function() {
-      gmap = new google.maps.Map(document.getElementById("js-map"), {
-        center: new google.maps.LatLng(33.646259, -117.842056),
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        zoom: 15,
-        panControl: false,
-        zoomControl: true,
-        zoomControlOptions: {
-          position: google.maps.ControlPosition.TOP_RIGHT,
-          style: google.maps.ZoomControlStyle.LARGE
-        },
-        styles: mapStyles
-      });
-    }
-  };
-}();
+};
 
 $(document).ready(function() {
   $(".js-select").chosen();
   AntNet.init();
+  google.maps.visualRefresh = true;
 });
