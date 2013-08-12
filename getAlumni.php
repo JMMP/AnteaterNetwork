5fe346eb78da1b766d374b98229d6574bb464d6b
suuoh <melvin.chien@gmail.com>
2013-08-12 18:14:34 -0400

    Add retrieval of categories and schools

--- getAlumni.php
+++ getAlumni.php
@@ -5,27 +5,85 @@ session_start();
  + Anteater Network v15.0
  + Copyright 2013 JMMP and UCI Alumni Association
  + http://alumni.uci.edu/anteater-network
-*/
+ */
 
 if (isset($_GET["antnet"])) {
   require_once("../../antnet_secure.php");
 
-// Execute query to select all rows
-  $result = mysqli_query($mysqli, "SELECT * FROM `" . $table . "` ORDER BY `Business_Name`");
+  // Get businesses
+  $result = mysqli_query($mysqli, 
+    "SELECT id, 
+    Last_Name,
+    First_Name,
+    Class_Year,
+    School_Code,
+    School_Name,
+    Business_Title,
+    Business_Name,
+    Business_Category,
+    Business_Street1,
+    Business_Street2,
+    Business_City,
+    Business_State,
+    Business_Country,
+    Business_Zipcode,
+    Business_Phone,
+    Business_Phone_Ext,
+    Business_Lat,
+    Business_Lng
+    FROM " . $table . 
+    " WHERE Business_Name != \"\" ORDER BY Business_Name");
 
   if (!$result) {
     if ($debug)
-      echo "Query failed: " . mysqli_error($mysqli);
+      echo "Businesses query failed: " . mysqli_error($mysqli);
     die();
   }
 
   $businesses = array();
-
-  while ($row = mysqli_fetch_assoc($result)) {
+  while ($row = mysqli_fetch_assoc($result))
     $businesses[] = $row;
+
+
+  // Get schools
+  $result = mysqli_query($mysqli,
+    "SELECT DISTINCT School_Name 
+    FROM " . $table . " ORDER BY School_Name");
+
+  if (!$result) {
+    if ($debug)
+      echo "Schools query failed: " . mysqli_error($mysqli);
+    die();
   }
 
-  echo json_encode($businesses);
+  $schools = array();
+  while ($row = mysqli_fetch_assoc($result))
+    $schools[] = $row;
+
+
+  // Get categories
+  $result = mysqli_query($mysqli,
+    "SELECT DISTINCT Business_Category 
+    FROM " . $table . " ORDER BY Business_Category");
+
+  if (!$result) {
+    if ($debug)
+      echo "Categories query failed: " . mysqli_error($mysqli);
+    die();
+  }
+
+  $categories = array();
+  while ($row = mysqli_fetch_assoc($result))
+    $categories[] = $row;
+
+  // Return all results as a JSON object
+  $data = array(
+    "businesses" => $businesses,
+    "schools" => $schools,
+    "categories" => $categories);
+
+  echo json_encode($data);
   mysqli_close($mysqli);
 }
+
 ?>
--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 session_start();
 
 /*
-+ Anteater Network v15.0
-+ Copyright 2013 JMMP and UCI Alumni Association
-+ http://alumni.uci.edu/anteater-network
-*/
+ + Anteater Network v15.0
+ + Copyright 2013 JMMP and UCI Alumni Association
+ + http://alumni.uci.edu/anteater-network
+ */
 
 $_SESSION["ip"] = $_SERVER["REMOTE_ADDR"];
 ?>
